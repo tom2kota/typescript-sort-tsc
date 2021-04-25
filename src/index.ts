14f6569b4f9a41fc9253dcae44cf062754e81410
Web Dev <tom2kota@gmail.com>
2021-04-25 21:18:27 +0300

    Add CharactersCollection sort class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import NumbersCollection from "./NumbersCollection";
 import Sorter from "./Sorter";
+import CharactersCollection from "./CharactersCollection";
 
 const inputData = [100, 3, -40, 2.5, 0, 10]
 const numbersCollection = new NumbersCollection(inputData)
-const sorter = new Sorter(numbersCollection)
+const dataSorter = new Sorter(numbersCollection)
 
 console.log(inputData)
 console.log('... sort() ...')
-sorter.sort()
+dataSorter.sort()
 console.log(numbersCollection.data)
+
+const inputText = 'Lorem ipsum gci skb Qa, DFD 2021, Zorro!'
+const charactersCollection = new CharactersCollection(inputText)
+const textSorter = new Sorter(charactersCollection)
+
+console.log(inputText)
+console.log('... sort() ...')
+textSorter.sort()
+console.log(charactersCollection.data)
