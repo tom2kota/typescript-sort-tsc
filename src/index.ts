b7b41739d11d635466f58722a957c5842ea6a840
Web Dev <tom2kota@gmail.com>
2021-04-25 22:11:18 +0300

    Add LinkedList sort class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import NumbersCollection from "./NumbersCollection";
 import Sorter from "./Sorter";
 import CharactersCollection from "./CharactersCollection";
+import LinkedList from "./LinkedList";
 
 const inputData = [100, 3, -40, 2.5, 0, 10]
 const numbersCollection = new NumbersCollection(inputData)
@@ -19,3 +20,17 @@ console.log(inputText)
 console.log('... sort() ...')
 textSorter.sort()
 console.log(charactersCollection.data)
+
+
+const linkedList = new LinkedList()
+linkedList.add(100)
+linkedList.add(-100)
+linkedList.add(50)
+linkedList.add(-200)
+linkedList.add(1100)
+
+const linkedListSorter = new Sorter(linkedList)
+linkedList.print()
+console.log('... sort() ...')
+linkedListSorter.sort()
+linkedList.print()
